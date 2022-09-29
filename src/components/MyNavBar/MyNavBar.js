2c0d12bad151acd5aa718d3b69cde0e3d8ef9455
NhatTan-uit <19522171@gm.uit.edu.vn>
2022-09-29 22:27:55 +0700

    add drop down menu

--- src/components/MyNavBar/MyNavBar.js
+++ src/components/MyNavBar/MyNavBar.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
@@ -6,14 +6,35 @@ import { Link } from 'react-router-dom';
 import './MyNavBar.css';
 
 function MyNavBar() {
+    const [dDMStatus, setDDMStatus] = useState('drop-down-menu-hidden');
+    const [dDMLabelStatus, setDDMLabelStatus] = useState('nav-drop-down-hidden');
+
     return (
         <Navbar bg="navcuatan" variant="light">
             <Container>
-                <Navbar.Brand style={{color: "navy"}} href="#home">Navbar</Navbar.Brand>
+                <Navbar.Brand style={{ color: "navy" }} href="#home">Navbar</Navbar.Brand>
                 <Nav className="me-auto">
-                    <Link to="/trang1" className='nav-mau-chu'>Trang 1</Link>
-                    <Link to="/trang2" className='nav-mau-chu'>Trang 2</Link>
-                    <Link to="/trang3" className='nav-mau-chu'>Trang 3</Link>
+                    <div className='nav-links'>
+                        <Link to="/trang1" className='nav-mau-chu'>Trang 1</Link>
+                        <Link to="/trang2" className='nav-mau-chu'>Trang 2</Link>
+                        <div
+                            onMouseLeave={() => {
+                                setDDMStatus('drop-down-menu-hidden');
+                                setDDMLabelStatus('nav-drop-down-hidden');
+                            }}
+                            onMouseEnter={() => {
+                                setDDMStatus('drop-down-menu');
+                                setDDMLabelStatus('nav-drop-down');
+                            }}
+                            className={dDMLabelStatus}>
+                            <Link
+                                to="/trang3"
+                                className='nav-mau-chu'>Drop down</Link>
+                            <div className={dDMStatus}>
+                                a
+                            </div>
+                        </div>
+                    </div>
                 </Nav>
             </Container>
         </Navbar>
